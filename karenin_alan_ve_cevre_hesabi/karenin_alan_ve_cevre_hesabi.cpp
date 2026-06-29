d9c6401c9b59d40297efe1a197d3e90fa2847812
Neval <neval.civan97@gmail.com>
2026-06-29 14:54:33 +0300

    Add files via upload

---
+++ karenin_alan_ve_cevre_hesabi/karenin_alan_ve_cevre_hesabi.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	int kenar;
+	cout << "Kenar degeri:";
+	cin >> kenar;
+	cout << "Karenin alani:" << kenar*kenar<<endl;
+	cout << "Karenin cevresi:" << kenar * 4;
+}
